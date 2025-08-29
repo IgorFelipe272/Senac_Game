88c433f86509e2229fd6aae27aabe32360d1b803
IgorFelipe272 <116975390+IgorFelipe272@users.noreply.github.com>
2025-08-29 09:32:44 -0300

    pra fazer o slide

---
+++ Assets/Scripts/ExemploCodigo.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ExemploCodigo : MonoBehaviour
+{
+    private void Awake()
+    {
+           
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
